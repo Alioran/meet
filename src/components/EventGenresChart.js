5a4c7b23f6035ff998093811ca62c6501c5ba499
Alioran <drakoronknight@gmail.com>
2023-10-19 17:20:13 -0700

    Exercise 4.10.5
    With notes

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -1,37 +1,47 @@
 import { useState, useEffect } from "react";
 import { PieChart, Pie, Legend, Cell, ResponsiveContainer } from "recharts";
 
+// define outside because it doesn't change ever
+const genres = ["React", "JavaScript", "Node", "jQuery", "Angular"];
+const colors = ["#a84740", "#dba51a", "#6cbd7a", "#6c95bd", "#906cbd"];
+
 const EventGenresChart = ({ events }) => {
   const [data, setData] = useState([]);
-  const genres = ["React", "JavaScript", "Node", "jQuery", "Angular"];
-  const colors = ['#a84740', '#dba51a', '#6cbd7a', '#6c95bd', '#906cbd'];
 
+  // simplified by putting getData inside useEffect, and then just change from function to variable
+  // to place inside setData
   useEffect(() => {
-    setData(getData());
-  }, [`${events}`]);
+    const data = genres.map((genre) => {
+      const filteredEvents = events.filter((event) =>
+        event.summary.includes(genre)
+      );
+      return {
+        name: genre,
+        value: filteredEvents.length,
+      };
+    });
+    setData(data);
+  }, [events]); // events dont need to be stringified to check for comparison because
+                // changes are being handled in App.js anyways
 
-  const getData = () => {
-    const data = genres.map(genre => {
-        const filteredEvents = events.filter(event => event.summary.includes(genre));
-        return {
-            name: genre,
-            value: filteredEvents.length
-          }
-    })
-      return data;
-  };
-
-  const renderCustomizedLabel = ({ cx, cy, midAngle, outerRadius, percent, index }) => {
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    outerRadius,
+    percent,
+    index,
+  }) => {
     const RADIAN = Math.PI / 180;
     const radius = outerRadius;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.20;
-    const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.20;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.2;
+    const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.2;
     return percent ? (
       <text
         x={x}
         y={y}
         fill={colors[index]}
-        textAnchor={x > cx ? 'start' : 'end'}
+        textAnchor={x > cx ? "start" : "end"}
         dominantBaseline="central"
       >
         {`${(percent * 100).toFixed(0)}%`}
@@ -49,21 +59,19 @@ const EventGenresChart = ({ events }) => {
           left: -30,
         }}
       >
-    <Pie
-         data={data}
-         dataKey="value"
-         fill="#8884d8"
-         labelLine={false}
-         label =  {renderCustomizedLabel}
-         outerRadius={120}           
-       >
-            {
-      data.map((entry, index) => (
-        <Cell key={`cell-${index}`} fill={colors[index]}/>
-      ))
-    }
-       </Pie>
-       <Legend verticalAlign="bottom" height={36}/>
+        <Pie
+          data={data}
+          dataKey="value"
+          fill="#8884d8"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={120}
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index]} />
+          ))}
+        </Pie>
+        <Legend verticalAlign="bottom" height={36} />
       </PieChart>
     </ResponsiveContainer>
   );
